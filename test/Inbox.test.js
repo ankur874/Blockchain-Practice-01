5f16fa7ce2ba77c0dbf6a415c665354309311f73
Ankur Attri <ankurattri874@gmail.com>
2023-02-17 21:07:59 +0530

    tested deployed contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,21 +1,33 @@
-const asset = require('assert');
+const assert = require('assert');
 const ganache = require('ganache');
 const Web3 = require('web3'); // W is capital in Web3 coz it's a constructor function 
-const web3 = new Web3(ganache.provider());// w is small in web3 coz it's an instance of 'Web3' .
+const provider = ganache.provider();
+const web3 = new Web3(provider);// w is small in web3 coz it's an instance of 'Web3' .
 const {abi,evm} = require('../compile');
 
 let bytecode = evm.bytecode.object;
 let accounts,inbox;
+const INITAL_MSG='Hi there!';
 
 beforeEach(async()=>{
     accounts =await web3.eth.getAccounts();//fetch all accounts
     inbox = await new web3.eth.Contract(abi)
-       .deploy({data:bytecode,arguments:['Hi there!']})
+       .deploy({data:bytecode,arguments:[INITAL_MSG]})
        .send({from:accounts[0],gas:'1000000'});
+    inbox.setProvider(provider);
 });
-describe("test",()=>{
-    it('test1',()=>{
-     console.log(inbox);
+describe("Inbox",()=>{
+    it('Is contract deployed?',()=>{
+       assert.ok(inbox.options.address);
+    });
+    it('Is initial message set?',async()=>{
+        const message=await inbox.methods.message().call();
+        assert.equal(message,INITAL_MSG);
+    })
+    it('Is setMessage working?',async()=>{
+      await inbox.methods.setMessage('bye').send({from:accounts[0]});
+      const message=await inbox.methods.message().call();
+      assert.equal(message,'bye');
     });
 });
 
